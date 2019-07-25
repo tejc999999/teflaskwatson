c877d28ca5a2161f8367a22fc60227f81c1ed0d1
tejc999999 <tejc999999@gmail.com>
2019-07-25 16:27:51 +0900

    2019/7時点仕様変更対応

--- flaskr/start.py
+++ flaskr/start.py
@@ -1,27 +1,33 @@
 # -*- coding: utf-8 -*-
 # 起動コマンド(Windows)
-# Flaskアプリケーションのエントリポイント
+# Flaskアプリケーションのエントリポイント（公開時は使用せず）
 # set FLASK_APP=flaskr
 # 動作環境
 # set FLASK_ENV=development
 # flask run
 from flask import Flask
 import os
+import watson
 
-# アプリケーション作成
-def create_app():
-    # Flaskアプリケーションインスタンス作成
-    # __name__:現在のPythonモジュール名
-    # insntace_relative_config=True:設定ファイルがインスタンスフォルダに関連(インスタンスフォルダから相対的にファイル参照)
-    app = Flask(__name__, instance_relative_config=True)
-    
-    import watson
-    app.register_blueprint(watson.bp)
-    # app.add_url_rule('/', endpoint='index')
-    
-    return app
-    
-port = int(os.getenv('PORT', 5000))
-if __name__ == '__main__':
-	app = create_app()
-	app.run(host='0.0.0.0', port=port, debug=True)
+app = Flask(__name__, instance_relative_config=True)
+app.register_blueprint(watson.bp)
+
+## アプリケーション作成
+#def create_app():
+#    # Flaskアプリケーションインスタンス作成
+#    # __name__:現在のPythonモジュール名
+#    # insntace_relative_config=True:設定ファイルがインスタンスフォルダに関連(インスタンスフォルダから相対的にファイル参照)
+#    app = Flask(__name__, instance_relative_config=True)
+#    
+#    import watson
+#    app.register_blueprint(watson.bp)
+#    # app.add_url_rule('/', endpoint='index')
+#    
+#    return app
+#    
+#port = int(os.getenv('PORT', 5000))
+#if __name__ == '__main__':
+#	app = create_app()
+#    # debug=True:デバッグ機能有効（ログ出力等）
+#    # host='0.0.0.0':ローカルマシン以外のアプリ接続を許可
+#	app.run(host='0.0.0.0', port=port, debug=True)
--- flaskr/watson.py
+++ flaskr/watson.py
@@ -31,49 +31,50 @@ def index():
         else:
             language = "ja"
         print("言語:", language)
-
 # Watson:Natural Language Understanding0
 # 日本語か英語かを分析
-#        from watson_developer_cloud import NaturalLanguageUnderstandingV1
-#        from watson_developer_cloud.natural_language_understanding_v1 import Features, EntitiesOptions
-#        from watson_developer_cloud.watson_service import WatsonApiException
-#        natural_language_understanding = NaturalLanguageUnderstandingV1(
-#          username='e6c17e9c-c00c-471b-8de0-ad5b33799eb3',
-#          password='HAHE0X4ZWyhR',
-#          version='2018-03-16')
-#        language='error'
-#        try:
-#            response = natural_language_understanding.analyze(
-#              text=comment,
-#              features=Features(
-#                  entities=EntitiesOptions(
-#                  emotion=True,
-#                  sentiment=True,
-#                  limit=2),
-#            ))
-#            language = response['language']
-#            print(json.dumps(language, indent=2))
-#        except WatsonApiException:
-#            print("NaturalLanguageUnderstanding error")
-#            messages=('入力言語を判断できませんでした',)
-#            print(language)
-
-#        if(language != 'error'):
-
+        """
+        from watson_developer_cloud import NaturalLanguageUnderstandingV1
+        from watson_developer_cloud.natural_language_understanding_v1 import Features, EntitiesOptions
+        from watson_developer_cloud.watson_service import WatsonApiException
+        natural_language_understanding = NaturalLanguageUnderstandingV1(
+          username='e6c17e9c-c00c-471b-8de0-ad5b33799eb3',
+          password='HAHE0X4ZWyhR',
+          version='2018-03-16')
+        language='error'
+        try:
+            response = natural_language_understanding.analyze(
+              text=comment,
+              features=Features(
+                  entities=EntitiesOptions(
+                  emotion=True,
+                  sentiment=True,
+                  limit=2),
+            )).get_result()
+            language = response['language']
+            print(json.dumps(language, indent=2))
+        except WatsonApiException:
+            print("NaturalLanguageUnderstanding error")
+            messages=('入力言語を判断できませんでした',)
+            print(language)
+        """
 # Watson:Language Translater
 # 翻訳処理(日本語->英語)：翻訳精度が悪い、Google翻訳を介した方が感情分析の精度は高い
+        """
         if(language == 'ja'):
             from watson_developer_cloud import LanguageTranslatorV3
             language_translator = LanguageTranslatorV3(
                     version='2018-05-01',
-                    iam_api_key='YRH0vpBeJhjmqAKNeR2OqSlPGlZMgIVi8cRjs1L4doPS')
+                    iam_apikey='YRH0vpBeJhjmqAKNeR2OqSlPGlZMgIVi8cRjs1L4doPS',
+                    url='https://gateway.watsonplatform.net/language-translator/api'
+                    )
             translation = language_translator.translate(
                     text=comment,
-                    model_id='ja-en')
+                    model_id='ja-en').get_result()
             comment = translation['translations'][0]['translation']
             print("Watson Language Transfer(json):", json.dumps(translation, indent=2))
             print('翻訳後：', comment)
-
+        """
 # Watson:Tone Analyzer    
 # 感情分析
         # Watson問い合わせ用インスタンス生成
@@ -81,10 +82,12 @@ def index():
         tone_analyzer = ToneAnalyzerV3(
                 version ='2017-09-21',
                 username ='fa76b991-555e-43a8-a205-7ce82f7db28b',
-                password ='qzLFvFo2LoTE')
+                password ='qzLFvFo2LoTE',
+                url='https://gateway.watsonplatform.net/tone-analyzer/api'
+        )
         text = comment
         content_type = 'application/json'
-        tone = tone_analyzer.tone({"text": text},content_type)
+        tone = tone_analyzer.tone({"text": text},content_type=content_type).get_result()
         print("Watson Tone Analyzer(json):", json.dumps(tone, indent=2))
         tones_data = tone['document_tone']['tones']
 
@@ -94,9 +97,10 @@ def index():
                       tone_data['score'])
         else:
             messages=('感情を含んだ文章を入力してください',)
-        
+        """
         if(len(tones_data) > 0):
             tone_data = tones_data[0]
+            print("tone_data:", tone_data)
             if(tone_data['tone_id'] == 'anger'):
                 images[0]['width'] = 100 * tone_data['score']
                 images[0]['height'] = 100 * tone_data['score']
@@ -112,7 +116,8 @@ def index():
             elif(tone_data['tone_id'] == 'sadness'):
                 images[4]['width'] = 100 * tone_data['score']
                 images[4]['height'] = 100 * tone_data['score']
-            
-        return render_template('watson/watson.html', images=images, messages=messages)
+        """
 
+        return render_template('watson/watson.html', messages=messages)
+ 
     return render_template('watson/watson.html')
